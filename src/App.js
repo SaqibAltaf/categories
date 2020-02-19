df02baede48c7363787eaaaa8011fe9eb29cd6b2
DH-01 <Saqib Altaf>
2020-02-19 19:03:08 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+const data = [
+  {
+    name: "Apple",
+    desc: "Apple category"
+  }, {
+    name: "Mango",
+    desc: "Mango category"
+  }, {
+    name: "Banana",
+    desc: "Banana category"
+  }, {
+    name: "Orange ",
+    desc: "Orange category"
+  }, {
+    name: "Grapes",
+    desc: "Grapes category"
+  },
+]
+function Categories({ props }) {
+  return (
+    props.map((val, key) => {
+      return (
+        <li key={key} onClick={() => this.callback(key)}>{val.name}</li>
+      )
+    })
+  )
+}
+
+
+function Category({ callback }) {
+const id = (id)=>callback(id);
+console.log(id)
+  return (
+    <h1></h1>
+
+  )
+}
+
+
+
+const callback1 = (id) => {
+  return id
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Categories props={data}></Categories>
+      <Category callback={callback1} />
     </div>
   );
 }
